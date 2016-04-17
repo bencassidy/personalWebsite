e02f64ede687d081882bf4628d87078a751cc86e
bencassidy <ben.cassidy11@gmail.com>
2016-04-17 14:28:56 -0700

    added bootstrap template

---
+++ server.js
@@ -0,0 +1,33 @@
+var hapi = require('hapi');
+var inert = require('inert');
+var nodemon = require('nodemon');
+
+var server = new hapi.Server();
+server.connection({ port: 80 });
+
+server.start( function (body) {
+    console.log('Server running at:', server.info.uri);
+    console.log(body);
+});
+
+server.register(inert, function (err) {
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./public/index.html');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/{filename*}',
+        handler: function (request, reply) {
+            reply.file('./public/' + request.params.filename);
+        }
+    });
+});
