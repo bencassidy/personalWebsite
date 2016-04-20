889c57381a65b53f2e0e2a1e120e24a134ab9d59
bencassidy <ben.cassidy11@gmail.com>
2016-04-19 17:02:28 -0700

    added email functionality

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var hapi = require('hapi');
 var inert = require('inert');
 var nodemon = require('nodemon');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport('smtps://ben.cassidy11%40gmail.com:APPPASSWORD@smtp.gmail.com');
 
 var server = new hapi.Server();
 server.connection({ port: 80 });
 
-server.start( function (body) {
+server.start( function () {
     console.log('Server running at:', server.info.uri);
-    console.log(body);
 });
 
 server.register(inert, function (err) {
@@ -25,9 +27,25 @@ server.register(inert, function (err) {
 
     server.route({
         method: 'POST',
-        path: '/contact_me.php',
+        path: '/mail',
         handler: function (request, reply) {
-            reply.file('./contact_me.php');
+            var mailOptions = {
+                from: request.payload.email, // sender address
+                to: 'ben.cassidy11@gmail.com', // list of receivers
+                subject: 'Email from bencassidy.com', // Subject line
+                text: 'From: ' +request.payload.email + '\n\n' +
+                      'Name: ' + request.payload.name + '\n\n' +
+                      'Message: ' + request.payload.message
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    return console.log(error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
+            reply("Success");
         }
     });
 
